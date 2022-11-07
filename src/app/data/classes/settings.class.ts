3d01f037d8cfabfc701e6555dae5d85e2b582242
deanbyrne95 <4136983+deanbyrne95@users.noreply.github.com>
2022-11-07 21:20:37 +0000

    Deploying to master from  @ 654c814112463485532938150a01af411e2d6c74 🚀

--- src/app/data/classes/settings.class.ts
+++
@@ -1,9 +0,0 @@
-export class Settings {
-    public sidebar: {
-        display: boolean;
-        minimised: boolean;
-    }
-    public displaySidebar: boolean;
-    public sidebarMinimised: boolean;
-    public theme: string;
-}
--- src/app/data/constants/constants.ts
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-export const SETTINGS = 'settings';
-
-export const LIGHT_THEME = 'light';
-export const DARK_THEME = 'dark';
